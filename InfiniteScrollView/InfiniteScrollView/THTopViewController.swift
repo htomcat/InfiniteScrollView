29fabce13560d229a4f94a140fad5b768e5ee6bc
Tomofumi Hamada- <thamada@yahoo-corp.jp>
2017-04-15 12:17:35 +0900

    [add]New TopVC

---
+++ InfiniteScrollView/InfiniteScrollView/THTopViewController.swift
@@ -0,0 +1,17 @@
+//
+//  THTopViewController.swift
+//  InfiniteScrollView
+//
+//  Created by 浜田　智史 on 2017/04/15.
+//  Copyright © 2017年 htomcat. All rights reserved.
+//
+
+import UIKit
+
+final class THTopViewController: UIViewController {
+
+    // MARK: - Override
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
